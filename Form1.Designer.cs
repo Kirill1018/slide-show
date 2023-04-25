3d2a437bb2775782655275dfbf9a677f213a38d5
Kirill1018 <115730059+Kirill1018@users.noreply.github.com>
2023-04-25 08:45:01 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace slide_show
+{
+    partial class Image_view
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.prev_file = new System.Windows.Forms.Button();
+            this.next_file = new System.Windows.Forms.Button();
+            this.departure = new System.Windows.Forms.Button();
+            this.pict_box = new System.Windows.Forms.PictureBox();
+            this.loading = new System.Windows.Forms.Button();
+            this.label = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pict_box)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // prev_file
+            // 
+            this.prev_file.Location = new System.Drawing.Point(188, 390);
+            this.prev_file.Name = "prev_file";
+            this.prev_file.Size = new System.Drawing.Size(82, 48);
+            this.prev_file.TabIndex = 1;
+            this.prev_file.Text = "предыдущий файл";
+            this.prev_file.UseVisualStyleBackColor = true;
+            this.prev_file.Click += new System.EventHandler(this.prev_file_Click);
+            // 
+            // next_file
+            // 
+            this.next_file.Location = new System.Drawing.Point(439, 390);
+            this.next_file.Name = "next_file";
+            this.next_file.Size = new System.Drawing.Size(75, 48);
+            this.next_file.TabIndex = 3;
+            this.next_file.Text = "следующий файл";
+            this.next_file.UseVisualStyleBackColor = true;
+            this.next_file.Click += new System.EventHandler(this.next_file_Click);
+            // 
+            // departure
+            // 
+            this.departure.Location = new System.Drawing.Point(308, 386);
+            this.departure.Name = "departure";
+            this.departure.Size = new System.Drawing.Size(75, 23);
+            this.departure.TabIndex = 4;
+            this.departure.Text = "сохранение";
+            this.departure.UseVisualStyleBackColor = true;
+            this.departure.Click += new System.EventHandler(this.departure_Click);
+            // 
+            // pict_box
+            // 
+            this.pict_box.Location = new System.Drawing.Point(49, 42);
+            this.pict_box.Name = "pict_box";
+            this.pict_box.Size = new System.Drawing.Size(681, 320);
+            this.pict_box.TabIndex = 5;
+            this.pict_box.TabStop = false;
+            this.pict_box.Paint += new System.Windows.Forms.PaintEventHandler(this.pict_box_Paint);
+            // 
+            // loading
+            // 
+            this.loading.Location = new System.Drawing.Point(308, 415);
+            this.loading.Name = "loading";
+            this.loading.Size = new System.Drawing.Size(75, 23);
+            this.loading.TabIndex = 2;
+            this.loading.Text = "загрузка";
+            this.loading.UseVisualStyleBackColor = true;
+            this.loading.Click += new System.EventHandler(this.loading_Click);
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(579, 405);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(35, 13);
+            this.label.TabIndex = 6;
+            this.label.Text = "label1";
+            // 
+            // Image_view
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.pict_box);
+            this.Controls.Add(this.departure);
+            this.Controls.Add(this.next_file);
+            this.Controls.Add(this.loading);
+            this.Controls.Add(this.prev_file);
+            this.Name = "Image_view";
+            this.Text = "image view";
+            ((System.ComponentModel.ISupportInitialize)(this.pict_box)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button prev_file;
+        private System.Windows.Forms.Button next_file;
+        private System.Windows.Forms.Button departure;
+        private System.Windows.Forms.PictureBox pict_box;
+        private System.Windows.Forms.Button loading;
+        private System.Windows.Forms.Label label;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace slide_show
+{
+    public partial class Image_view : Form
+    {
+        public Image_view()
+        {
+            InitializeComponent();
+        }
+        List<Image> images = new List<Image>();
+        FolderBrowserDialog dialog = new FolderBrowserDialog();
+        Image illustration;
+        OpenFileDialog open_dial = new OpenFileDialog();
+        static Graphics graphic;
+        static Rectangle rect = new Rectangle();
+        PaintEventArgs args = new PaintEventArgs(graphic, rect);
+        int seek = 0;//индекс коллекции изображений
+        bool workload = false;
+        void view_img(bool log_val)
+        {
+            if (images.Count > 0)
+            {
+                if (log_val) seek++;
+                else seek--;
+                if (seek < 0) seek++;
+                if (seek == images.Count) seek--;
+                pict_box.Image = images[seek];
+                label.Text = $"{images[seek].ToString()}/{images.Count}";
+            }
+        }
+
+        private void prev_file_Click(object sender, EventArgs e)
+        {
+            view_img(false);
+            label.Text = $"{images[seek--].ToString()}/{images.Count}";
+        }
+
+        private void next_file_Click(object sender, EventArgs e)
+        {
+            view_img(true);
+        }
+
+        private void pict_box_Paint(object sender, PaintEventArgs e)
+        {
+            open_dial.Filter = "файлы изображений|*.bmp;*.png;*.jpg";
+            if (open_dial.ShowDialog() != DialogResult.OK) return;
+            try
+            {
+                illustration = Image.FromFile(open_dial.FileName);
+            }
+            catch (OutOfMemoryException exception)
+            {
+                MessageBox.Show("ошибка чтения картинки");
+                return;
+            }
+            if (illustration == null) return;
+            e.Graphics.DrawImage(illustration, 0, 0);
+            images.Add(illustration);
+        }
+
+        private void departure_Click(object sender, EventArgs e)
+        {
+            dialog.ShowDialog();
+        }
+
+        private void loading_Click(object sender, EventArgs e)
+        {
+            open_dial.Filter = "файлы изображений|*.bmp;*.png;*.jpg";
+            if (open_dial.ShowDialog() != DialogResult.OK) return;
+            try
+            {
+                illustration = Image.FromFile(open_dial.FileName);
+            }
+            catch (OutOfMemoryException exception)
+            {
+                MessageBox.Show("ошибка чтения картинки");
+                return;
+            }
+            if (illustration == null) return;
+            args.Graphics.DrawImage(illustration, 0, 0);
+            images.Add(illustration);
+        }
+    }
+}
